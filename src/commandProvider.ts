49d0a2829a0801f440b66a81ef33708dbbb67331
Box-Of-Hats <ja1ke@hotmail.co.uk>
2021-06-05 22:06:20 +0100

    #6 if no selection is made, default to whole document

--- src/commandProvider.ts
+++ src/commandProvider.ts
@@ -2,50 +2,52 @@ import * as vscode from "vscode";
 import { convertInterfacesToCSharp } from "ts-csharp";
 
 class TsCsharpCommandProvider {
-    generateClassFromInterfaces = (exportedInterfacesOnly?: boolean) => {
-        let textEditor = vscode.window.activeTextEditor;
+	generateClassFromInterfaces = (exportedInterfacesOnly?: boolean) => {
+		let textEditor = vscode.window.activeTextEditor;
 
-        let documentText = textEditor!.document.getText(textEditor!.selection);
+		if (!textEditor) {
+			vscode.window.showErrorMessage(
+				"No active text editor. Please open a file"
+			);
+			return;
+		}
 
-        if (!textEditor) {
-            vscode.window.showErrorMessage(
-                "No active text editor. Please open a file"
-            );
-            return;
-        }
+		const documentText =
+			textEditor.document.getText(textEditor.selection) ||
+			textEditor.document.getText();
 
-        let infoMessage = vscode.window.setStatusBarMessage(
-            `Generating csharp class...`
-        );
+		let infoMessage = vscode.window.setStatusBarMessage(
+			`Generating csharp class...`
+		);
 
-        let csharpClass: string = convertInterfacesToCSharp(
-            documentText,
-            exportedInterfacesOnly
-        );
+		let csharpClass: string = convertInterfacesToCSharp(
+			documentText,
+			exportedInterfacesOnly
+		);
 
-        if (!csharpClass || csharpClass.length === 0) {
-            infoMessage.dispose();
-            vscode.window.showErrorMessage(
-                "Could not find any interfaces to generate from."
-            );
-        }
+		if (!csharpClass || csharpClass.length === 0) {
+			infoMessage.dispose();
+			vscode.window.showErrorMessage(
+				"Could not find any interfaces to generate from."
+			);
+		}
 
-        vscode.workspace
-            .openTextDocument({
-                language: "csharp",
-                content: csharpClass
-            })
-            .then(doc => {
-                vscode.window
-                    .showTextDocument(doc)
-                    .then(e => {
-                        vscode.commands.executeCommand(
-                            "editor.action.formatDocument"
-                        );
-                    })
-                    .then(infoMessage.dispose());
-            });
-    }
+		vscode.workspace
+			.openTextDocument({
+				language: "csharp",
+				content: csharpClass,
+			})
+			.then((doc) => {
+				vscode.window
+					.showTextDocument(doc)
+					.then((e) => {
+						vscode.commands.executeCommand(
+							"editor.action.formatDocument"
+						);
+					})
+					.then(infoMessage.dispose());
+			});
+	};
 }
 
 export { TsCsharpCommandProvider };
